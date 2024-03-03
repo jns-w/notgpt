0860ef716bb05002bdaa945ca779853dd0aaaa87
jns-w <jns.w@icloud.com>
2024-03-03 15:04:03 +0800

    production implementation

--- notgpt-cli/vite.config.ts
+++ notgpt-cli/vite.config.ts
@@ -1,15 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react()],
+  preview: {
+    port: 80,
+    strictPort: true,
+  },
   server: {
-    proxy: {
-      "/api": {
-        target: 'http://127.0.0.1:8080',
-        changeOrigin: true,
-      }
-    }
-  }
-})
+    port: 80,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:8080",
+  },
+});
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
+//
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       "/api": {
+//         target: 'http://127.0.0.1:8080',
+//         changeOrigin: true,
+//       }
+//     }
+//   }
+// })
